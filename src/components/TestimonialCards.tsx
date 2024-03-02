214f2ea7cb4a35aee0141158e19a6aa3a5cfdd08
Shivam <jainshivam2204@gmail.com>
2024-03-02 14:32:28 +0530

    TestimonialCards updated

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -4,33 +4,33 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 const musicTestimonials = [
     {
       quote:
-        'Joining the music school transformed my understanding of music and helped me to truly discover my own sound. The instructors are world-class!',
+        'I started with minimal coding knowledge and enrolled in their programming courses. Completed the app development program, and now my app has thousands of downloads and positive reviews!',
       name: 'Alex Johnson',
-      title: 'Guitar Student',
+      title: 'App Development Student',
     },
     {
       quote:
-        "The community and support at this school are unmatched. I've grown not just as a pianist, but also as a performer, thanks to their comprehensive approach.",
+        "I joined their cybersecurity certification program to delve into ethical hacking and network security. Now, I'm working as a junior cybersecurity analyst with a top tech firm thanks to the skills I acquired.",
       name: 'Samantha Lee',
-      title: 'Piano Student',
+      title: 'Cyber Security Student',
     },
     {
       quote:
-        "This school offered me the tools and confidence to take my singing to the next level. I'm endlessly grateful for the personalized coaching.",
+        "Immersed myself in AI and machine learning courses, gaining practical skills. Now, I'm working as a machine learning engineer at a prominent tech company, turning my passion into a profession.",
       name: 'Michael Chen',
-      title: 'Vocal Student',
+      title: 'Machine Learning Student',
     },
     {
       quote:
-        'As a violinist, finding the right mentor can be challenging, but this school matched me with a teacher who truly understands my goals and challenges.',
+        'Transitioned from a non-tech background to the IT industry with their career transition program. Landed a role as a junior software developer at a startup, proving that career shifts are possible.',
       name: 'Emily Taylor',
-      title: 'Violin Student',
+      title: 'Web Development Student',
     },
     {
       quote:
-        'The production courses here opened my eyes to the intricacies of music production. Highly recommend for any aspiring producers!',
+        'Dived into blockchain courses to understand the future of finance. Now actively contributing to blockchain projects, influencing the financial landscape with distributed ledger technology.',
       name: 'Chris Morales',
-      title: 'Music Production Student',
+      title: 'Blockchain Student',
     },
   ];
 
@@ -38,15 +38,19 @@ function TestimonialCards() {
     
   return (
     <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-      <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
+
+      <h2 className="text-3xl font-bold text-center mb-8 z-10">Unlock Success: Witness the Achievements of Our Learners</h2>
+
       <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
+
         <div className="w-full max-w-6xl">
-        <InfiniteMovingCards
-        items={musicTestimonials}
-        direction="right"
-        speed="slow"
-      />
+          <InfiniteMovingCards
+          items={musicTestimonials}
+          direction="right"
+          speed="slow"
+          />
         </div>
+        
       </div>
     </div>
   )
